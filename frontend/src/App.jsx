8f6200497c0bfc3ef22a672aa13b0db67b8627b1
Prince <princejaiswal13206@gmail.com>
2024-04-28 20:25:35 +0530

    Blog page modification (Prince)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import CropSuggestion from './Components/CropSuggestion';
 import ShopPage from './Components/Shop';
-import Blog from './Components/BlogPage/BlogPage'
 import BlogPage from './Components/BlogPage/BlogPage';
+import Blogs from './Components/BlogPage/Blogs'; // Import Blogs component
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <Router>
             <Navbar />
             <Routes>
                 <Route path="/crop-suggestion" element={<CropSuggestion />} />
                 <Route path="/shop" element={<ShopPage />} /> 
-                <Route path="/Blog" element={<BlogPage />} /> 
+                <Route path="/blog" element={<BlogPage />} />
+                <Route path="/blog/:id" element={<Blogs />} /> {/* Route for detailed blog page */}
             </Routes>
-        </BrowserRouter>
+        </Router>
     );
 };
 
